8ef637b278a3ccf72ab5a0ea368a2d7348772b23
Prikindel <kiryakovm96@gmail.com>
2020-04-12 22:39:32 +0300

    Data layer. Added features: Register, Update token

---
+++ app/src/main/java/com/invaderprogrammer/android/chat/data/account/AccountCache.kt
@@ -0,0 +1,10 @@
+package com.invaderprogrammer.android.chat.data.account
+
+import com.invaderprogrammer.android.chat.domain.type.Either
+import com.invaderprogrammer.android.chat.domain.type.None
+import com.invaderprogrammer.android.chat.domain.type.exception.Failure
+
+interface AccountCache {
+    fun getToken(): Either<Failure, String>
+    fun saveToken(token: String): Either<Failure, None>
+}
---
+++ app/src/main/java/com/invaderprogrammer/android/chat/data/account/AccountRemote.kt
@@ -0,0 +1,15 @@
+package com.invaderprogrammer.android.chat.data.account
+
+import com.invaderprogrammer.android.chat.domain.type.Either
+import com.invaderprogrammer.android.chat.domain.type.None
+import com.invaderprogrammer.android.chat.domain.type.exception.Failure
+
+interface AccountRemote {
+    fun register(
+        email: String,
+        name: String,
+        password: String,
+        token: String,
+        userDate: Long
+    ): Either<Failure, None>
+}
