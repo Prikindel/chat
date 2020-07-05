562a69d38acaa90ebffe9a1b0584f42d5274ab85
Prikindel <kiryakovm96@gmail.com>
2020-07-05 20:35:18 +0300

    Cache layer. Added: logout, get account, save account

--- app/src/main/java/com/invaderprogrammer/android/chat/cache/AccountCacheImpl.kt
+++ app/src/main/java/com/invaderprogrammer/android/chat/cache/AccountCacheImpl.kt
@@ -1,6 +1,7 @@
 package com.invaderprogrammer.android.chat.cache
 
 import com.invaderprogrammer.android.chat.data.account.AccountCache
+import com.invaderprogrammer.android.chat.domain.account.AccountEntity
 import com.invaderprogrammer.android.chat.domain.type.Either
 import com.invaderprogrammer.android.chat.domain.type.None
 import com.invaderprogrammer.android.chat.domain.type.Failure
@@ -14,5 +15,17 @@ class AccountCacheImpl @Inject constructor(private val prefsManager: SharedPrefs
 
     override fun getToken(): Either<Failure, String> {
         return prefsManager.getToken()
+    }
+
+    override fun logout(): Either<Failure, None> {
+        return prefsManager.removeAccount()
+    }
+
+    override fun getCurrentAccount(): Either<Failure, AccountEntity> {
+        return prefsManager.getAccount()
+    }
+
+    override fun saveAccount(account: AccountEntity): Either<Failure, None> {
+        return prefsManager.saveAccount(account)
     }
 }
--- app/src/main/java/com/invaderprogrammer/android/chat/cache/SharedPrefsManager.kt
+++ app/src/main/java/com/invaderprogrammer/android/chat/cache/SharedPrefsManager.kt
@@ -1,6 +1,7 @@
 package com.invaderprogrammer.android.chat.cache
 
 import android.content.SharedPreferences
+import com.invaderprogrammer.android.chat.domain.account.AccountEntity
 import com.invaderprogrammer.android.chat.domain.type.Either
 import com.invaderprogrammer.android.chat.domain.type.None
 import com.invaderprogrammer.android.chat.domain.type.Failure
@@ -9,6 +10,12 @@ import javax.inject.Inject
 class SharedPrefsManager @Inject constructor(private val prefs: SharedPreferences) {
     companion object {
         const val ACCOUNT_TOKEN = "account_token"
+        const val ACCOUNT_ID = "account_id"
+        const val ACCOUNT_NAME = "account_name"
+        const val ACCOUNT_EMAIL = "account_email"
+        const val ACCOUNT_STATUS = "account_status"
+        const val ACCOUNT_DATE = "account_date"
+        const val ACCOUNT_IMAGE = "account_image"
     }
 
     fun saveToken(token: String): Either<Failure, None> {
@@ -20,6 +27,59 @@ class SharedPrefsManager @Inject constructor(private val prefs: SharedPreference
     }
 
     fun getToken(): Either<Failure, String> {
-        return Either.Right(prefs.getString(ACCOUNT_TOKEN, "") ?: "")
+        return Either.Right(prefs.getString(ACCOUNT_TOKEN, ""))
     }
+
+    fun saveAccount(account: AccountEntity): Either<Failure, None> {
+        prefs.edit().apply {
+            putLong(ACCOUNT_ID, account.id)
+            putString(ACCOUNT_NAME, account.name)
+            putString(ACCOUNT_EMAIL, account.email)
+            putString(ACCOUNT_TOKEN, account.token)
+            putString(ACCOUNT_STATUS, account.status)
+            putLong(ACCOUNT_DATE, account.userDate)
+            putString(ACCOUNT_IMAGE, account.image)
+        }.apply()
+
+        return Either.Right(None())
+    }
+
+    fun getAccount(): Either<Failure, AccountEntity> {
+        val id = prefs.getLong(ACCOUNT_ID, 0)
+
+        if (id == 0L) {
+            return Either.Left(Failure.NoSavedAccountsError)
+        }
+
+        val account = AccountEntity(
+            prefs.getLong(ACCOUNT_ID, 0),
+            prefs.getString(ACCOUNT_NAME, ""),
+            prefs.getString(ACCOUNT_EMAIL, ""),
+            prefs.getString(ACCOUNT_TOKEN, ""),
+            prefs.getString(ACCOUNT_STATUS, ""),
+            prefs.getLong(ACCOUNT_DATE, 0),
+            prefs.getString(ACCOUNT_IMAGE, "")
+        )
+
+        return Either.Right(account)
+    }
+
+    fun removeAccount(): Either<Failure, None> {
+        prefs.edit().apply {
+            remove(ACCOUNT_ID)
+            remove(ACCOUNT_NAME)
+            remove(ACCOUNT_EMAIL)
+            remove(ACCOUNT_STATUS)
+            remove(ACCOUNT_DATE)
+            remove(ACCOUNT_IMAGE)
+        }.apply()
+
+        return Either.Right(None())
+    }
+
+    fun containsAnyAccount(): Boolean {
+        val id = prefs.getLong(ACCOUNT_ID, 0)
+        return id != 0L
+    }
+
 }
